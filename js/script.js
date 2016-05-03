596e7d6efbf90c6bdc73ec07d82a69890e8a1114
feralislatr <slate@udel.edu>
2016-05-03 19:32:32 +0000

    added scrolling header

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 $(function() {
 	$('#go').bind('click',function(event){
-	    var $anchor = $(this);
-	    
+	    var anchor = $(this);
+
 		$('html, body').stop().animate({
-			scrollTop: $($anchor.attr('href')).offset().top
+			scrollTop: $(anchor.attr('href')).offset().top
 		}, 1300,'easeInOutExpo');
 		event.preventDefault();
 
 	});
 });
 
+$(function() {
+    var nav = $("nav"),
+        clone = nav.before(nav.clone().addClass("clone"));
+        
+    $(window).on("scroll", function() {
+         var fromTop = $(window).scrollTop();
+         $("body").toggleClass("down", (fromTop > 150));
+    });
+});
